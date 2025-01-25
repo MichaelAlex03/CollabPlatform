0b9cf343d7afb317214d693eacc3a5749944077b
Your Name <michaelalex03@outlook.com>
2025-01-25 17:52:19 -0600

    handle new student creation

--- studentBackend/controller/auth/registerController.js
+++ studentBackend/controller/auth/registerController.js
@@ -2,10 +2,24 @@ const bcrypt = require('bcrypt');
 const Student = require('../../model/Student');
 
 const handleNewStudent = async () => {
-    const {user, pass, netId, email} = req.body;
-    if (!user || !pass || !netId || !email) return res.status(400).json({message: 'missing fields'});
+    const { user, pass, netId, email } = req.body;
+    if (!user || !pass || !netId || !email) return res.status(400).json({ message: 'missing fields' });
 
-    
+    const duplicate = await Student.findOne({ username: user }).exec();
+    if (duplicate) return res.sendStatus(409);
+
+    try {
+        const hashedPwd = await bcrypt.hash(pass, 10);
+        await Student.create({
+            username: user,
+            password: hashedPwd,
+            netId,
+            email,
+        });
+        res.status(201).json({ message: `New student ${user} created` });
+    } catch (error) {
+        res.staus(500).json({ message: error.message });
+    }
 }
 
 module.exports = {
