08d3acdfcdf82a44c9e1d4195faca0fb3bc4710a
Your Name <michaelalex03@outlook.com>
2025-01-25 18:00:09 -0600

    handle cookies from request added to server.js also added corsOptions

--- studentBackend/server.js
+++ studentBackend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const corsOptions = require('./config/corsOptions');
+const credentials = require('./middleware/credentials')
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3000;
@@ -10,10 +12,16 @@ const app = express();
 connectDB();
 
 //Verifies request is coming from accepted origin
-app.use(cors());
+app.use(cors(corsOptions));
+
+//Allows for credentials to be sent back (Cookies)
+app.use(credentials());
 
 //Parses request body into JSON for every request
 app.use(express.json());
+
+//Parses cookies from request
+app.use(cookieParser());
 
 app.use('/auth/login', require('./routes/auth/login'));
 app.use('/auth/register', require('./routes/auth/register'));
