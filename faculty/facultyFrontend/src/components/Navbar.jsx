3e84f38ec4c6c714920114468ebe5d40979a8099
Michael A Lleverino <michaelalleverino@Michaels-MacBook-Pro.local>
2025-02-04 13:58:46 -0600

    removed unused elements in navbar

--- faculty/facultyFrontend/src/components/Navbar.jsx
+++ faculty/facultyFrontend/src/components/Navbar.jsx
@@ -13,7 +13,6 @@ const Navbar = () => {
     return (
         <header class="bg-white border-b-4 border-[#501214] sticky top-0 z-50 shadow-md">
             <nav class="max-w-6xl mx-auto p-4 flex justify-between items-center">
-                {/* <a href="#" class="text-[#501214] text-xl font-semibold no-underline">Texas State University</a> */}
                 <button onClick={() => handleClick('')}  class="text-[#501214] text-xl font-semibold no-underline">Texas State University</button>
                 <div class="hidden md:flex gap-8">
                     <button onClick={() => handleClick('overview')} class="text-gray-800 font-medium hover:text-[#501214]">Overview</button>
