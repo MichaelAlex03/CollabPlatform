1b49b91a0f99b0da46e2bf9894d4af8c5592cded
Olivia Laurel <OLaurel113@gmail.com>
2025-02-04 20:14:24 -0600

    adding in a login button

--- faculty/facultyFrontend/src/components/Navbar.jsx
+++ faculty/facultyFrontend/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const navigate = useNavigate();
 
     const handleClick = (div) => {
         if (!div){
@@ -10,15 +12,20 @@ const Navbar = () => {
         }
     }
 
+    const handleLoginClick = () => {
+        navigate('/login');
+    }
+
     return (
-        <header class="bg-white border-b-4 border-[#501214] sticky top-0 z-50 shadow-md">
-            <nav class="max-w-6xl mx-auto p-4 flex justify-between items-center">
-                <button onClick={() => handleClick('')}  class="text-[#501214] text-xl font-semibold no-underline">Texas State University</button>
-                <div class="hidden md:flex gap-8">
-                    <button onClick={() => handleClick('overview')} class="text-gray-800 font-medium hover:text-[#501214]">Overview</button>
-                    <button onClick={() => handleClick('process')} class="text-gray-800 font-medium hover:text-[#501214]">Project</button>
-                    <button onClick={() => handleClick('benefits')} class="text-gray-800 font-medium hover:text-[#501214]">Benefits</button>
-                    <button onClick={() => handleClick('faq')} class="text-gray-800 font-medium hover:text-[#501214]">FAQ</button>
+        <header className="bg-white border-b-4 border-[#501214] sticky top-0 z-50 shadow-md">
+            <nav className="max-w-6xl mx-auto p-4 flex justify-between items-center">
+                <button onClick={() => handleClick('')} className="text-[#501214] text-xl font-semibold no-underline">Texas State University</button>
+                <div className="hidden md:flex gap-8">
+                    <button onClick={() => handleClick('overview')} className="text-gray-800 font-medium hover:text-[#501214]">Overview</button>
+                    <button onClick={() => handleClick('process')} className="text-gray-800 font-medium hover:text-[#501214]">Project</button>
+                    <button onClick={() => handleClick('benefits')} className="text-gray-800 font-medium hover:text-[#501214]">Benefits</button>
+                    <button onClick={() => handleClick('faq')} className="text-gray-800 font-medium hover:text-[#501214]">FAQ</button>
+                    <button onClick={handleLoginClick} className="text-gray-800 font-medium hover:text-[#501214]">Log In</button>
                 </div>
             </nav>
         </header>
