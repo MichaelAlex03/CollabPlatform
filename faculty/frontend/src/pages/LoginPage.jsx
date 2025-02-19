0967370d85b00fd15fda3e970923b0a3dd119774
MichaelAlex03 <michaelalex03@outlook.com>
2025-02-19 13:38:35 -0600

    made handle login that makes api call to backend and checks db for user

--- faculty/frontend/src/pages/LoginPage.jsx
+++ faculty/frontend/src/pages/LoginPage.jsx
@@ -12,16 +12,34 @@ const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
-    const handleLogin = () => {
+    const [errMsg, setErrMsg] = useState('');
 
-        axios.post(LOGIN_URL, {
-            email,
-            pass
-        })
-        navigate('/')
-        setEmail('');
-        setPass('');
-        
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        try {
+            const response = await axios.post(LOGIN_URL, {
+                email,
+                pass
+            })
+            console.log(response)
+            navigate('/')
+            setEmail('');
+            setPass('');
+        } catch (err) {
+            console.log(err)
+            if (!err?.response) {
+                setErrMsg('No response from server');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Username and/or Password is wrong');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Email and Password required');
+            } else {
+                setErrMsg('Registration Failed');
+            }
+        }
+
+
     }
 
     const handleSignUpClick = () => {
@@ -34,14 +52,15 @@ const LoginPage = () => {
             <div className="flex justify-center items-center h-screen bg-zinc-100 p-6">
                 <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
                     <h2 className="text-2xl text-center font-semibold mb-6">Log In</h2>
+                    {errMsg && <p className='text-center text-sm md:text-base font-bold text-red-500 m-2'>{errMsg}</p>}
                     <form>
                         <div className="mb-4">
                             <label className="text-black text-sm md:text-base lg:text-lg">Email</label>
-                            <input 
-                                type="email" 
+                            <input
+                                type="email"
                                 className="w-full p-2 border border-gray-300 rounded mt-1"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)} 
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
                         <div className="mb-4">
@@ -58,7 +77,7 @@ const LoginPage = () => {
                             <label className="text-black text-sm mr-auto md:text-base lg:text-lg">Remember Me</label>
                             <label><a href="#" className="text-black text-sm mr-auto md:text-base lg:text-lg">Forgot Password?</a></label>
                         </div>
-                        <button type="submit" className="w-full bg-[#501214] hover:bg-[#7d1c1f] text-white p-2 rounded  text-md md:text-base lg:text-lg" onClick={() => handleLogin}>Log In</button>
+                        <button type="submit" className="w-full bg-[#501214] hover:bg-[#7d1c1f] text-white p-2 rounded  text-md md:text-base lg:text-lg" onClick={handleLogin}>Log In</button>
                         <label className="block text-center text-gray-700 mt-2 text-md md:text-base lg:text-lg">Don't have an account? <button onClick={handleSignUpClick} className="text-gray-700 mt-4">Sign Up</button> </label>
                     </form>
                 </div>
