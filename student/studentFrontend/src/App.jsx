1c274edb6eb09e9ffa2caa054888bc8a44520db6
Olivia Laurel <OLaurel113@gmail.com>
2025-02-24 16:15:11 -0600

    adding in the signin/login

--- student/studentFrontend/src/App.jsx
+++ student/studentFrontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage />}/>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   )
