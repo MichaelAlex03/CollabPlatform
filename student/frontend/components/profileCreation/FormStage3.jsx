871f25f5eec2da82df08ea0efaa788c1e606de24
Your Name <michaelalex03@outlook.com>
2025-02-26 12:47:39 -0600

    issue where adding link causes horizontal scrolling

--- student/frontend/components/profileCreation/FormStage3.jsx
+++ student/frontend/components/profileCreation/FormStage3.jsx
@@ -10,26 +10,36 @@ const FormStage3 = () => {
         const linkFields = [];
         for (let i = 0; i < numOfLinks; i++) {
             linkFields.push(
-                <div key={i} className='w-full flex flex-col items-start'>
-                    <label htmlFor={i} className='text-sm mt-2'>Link {i + 1}</label>
+                <div key={i} className='w-full flex flex-col items-start mb-2'>
+                    <label htmlFor={i} className='text-sm mt-2 font-semibold'>Link {i + 1}</label>
                     <div className='flex flex-row w-full items-center'>
                         <input type='text' id={i} className={`border-1 border-gray-400 p-2 rounded-lg w-full mt-1 ${i === 0 ? 'mr-0' : 'mr-5'}`}></input>
                         {i > 0 && (
                             <button
                                 type='button'
-                                onClick={() => setNumOfLinks(numOfLinks - 1)} className='transition duration-300 hover:scale-110'
+                                onClick={() => setNumOfLinks(numOfLinks - 1)}
+                            >
+                                <FontAwesomeIcon
+                                    icon={faTrash}
+                                    color='black'
+                                   
+                                    className='h-[10px]'
+                                />
+                            </button>
+                        )}
+                        {/* <button
+                                type='button'
+                                onClick={() => setNumOfLinks(numOfLinks - 1)}
                             >
                                 <FontAwesomeIcon
                                     icon={faTrash}
                                     color='black'
                                     size='lg'
-
                                 />
-                            </button>
-                        )}
+                            </button> */}
                     </div>
                     {i < 2 && (
-                        <p className='text-xs mt-2 hover:underline' onClick={() => setNumOfLinks(numOfLinks + 1)}>+ Add Another Link</p>
+                        <p className='text-xs mt-2 hover:underline font-semibold' onClick={() => setNumOfLinks(numOfLinks + 1)}>+ Add Another Link</p>
                     )}
                 </div>
             )
@@ -56,8 +66,8 @@ const FormStage3 = () => {
                 <textarea id='jobs' className='border-1 border-gray-400 p-2 rounded-lg w-full mt-1' />
             </div>
 
-            <div className='flex flex-col items-start w-full '>
-                <label htmlFor='links' className='text-sm md:text-base '>Links? Github, LinkedIn, Personal Site, etc...</label>
+            <div className='flex flex-col items-start w-full'>
+                <label className='text-sm md:text-base '>Links? Github, LinkedIn, Personal Site, etc...</label>
                 {renderLinkFields()}
             </div>
 
