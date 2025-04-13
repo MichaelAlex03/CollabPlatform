4ad619e27f428568f0dc548ef1d4940a8952cadf
MichaelAlex03 <michaelalex03@outlook.com>
2025-04-13 14:27:35 -0500

    updating cors url

--- student/backend/config/allowedOrigins.js
+++ student/backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'https://collab-platform-rn5wpgcq6-michael-lleverinos-projects.vercel.app',
-    'collab-platform-ten.vercel.app'
+    'https://collab-platform-ten.vercel.app'
 ]
 
 module.exports = allowedOrigins;
