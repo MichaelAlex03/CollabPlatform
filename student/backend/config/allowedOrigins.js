2fc2aec281fecb2139729a43d00acb3a175be4ac
MichaelAlex03 <michaelalex03@outlook.com>
2025-04-13 14:15:05 -0500

    updated vercel url

--- student/backend/config/allowedOrigins.js
+++ student/backend/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://collab-platform-rn5wpgcq6-michael-lleverinos-projects.vercel.app/'
+    'https://collab-platform-rn5wpgcq6-michael-lleverinos-projects.vercel.app'
 ]
 
 module.exports = allowedOrigins;
